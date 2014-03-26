73d2b015272d8366ab5b904a14b8ada9314563ed
MohamedMB <mohamed.maktouf@gmail.com>
2014-03-26 20:08:42 +0100

    Nettoyage des test. Gestion d'un thread de connexion commune au pool et à l'accès direct (3 fichiers dans connexionBDTest).

--- src/connexionBDTest/PoolConnexionTest.java
+++ src/connexionBDTest/PoolConnexionTest.java
@@ -1,12 +1,16 @@
 package connexionBDTest;
 
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
-
-import client.UtilisateurPoolConnexionThead;
 import fr.miage.facebook.pool.homemade.CustomConnectionPoolImpl;
 
 
@@ -19,64 +23,82 @@ import fr.miage.facebook.pool.homemade.CustomConnectionPoolImpl;
  
 public class PoolConnexionTest {
 
-	private static final int NB_UTISATEUR = 2_000;
-	private static final int NB_QUERY = 3;
-
-
 	private static final String DRIVER = "com.mysql.jdbc.Driver";
 	  private static final String URL = "jdbc:mysql://127.0.0.1:3306/facebook";
 	private static final String USER_NAME = "root";
-	private static final String PASSWORD = "mysql";
-	private static final int MAX_CONNECTIONS = 15;
-	private static final int INITIAL_CONNECTIONS = 2;
+	private static final String PASSWORD = "";
+	//3020 limite ordinateur mohamed, à changer !!!!!!!!!!!!!!!
+	private static final int MAX_CONNECTIONS = 1000; //limite qu'il ne dépasse pas !
+	private static final int INITIAL_CONNECTIONS = 1000; //créé les connexion sur place
 	private static final boolean WAIT_IF_BUSY = true;
-
-
-
-
-
+	
+	private ArrayList<HashMap<Integer, Long>> dureesConnexions = null;
+	
+	private static final boolean UTILISER_THREAD = false;
 
 	private Logger logger = Logger.getLogger(getClass());
 
 
 	@Test
 	public void connexionBDTest(){
-
+		long beginGlobal;
+		long endGlobal;
+		
+		int palier = 200;
+		
 		long begin = 0;
 		long end;
-		int nbUtilisateur = 0;
-
-		begin = System.currentTimeMillis();
-
-		try{
-			for ( nbUtilisateur = 1; nbUtilisateur <= NB_UTISATEUR ; nbUtilisateur++){
-
-				CustomConnectionPoolImpl pool = new CustomConnectionPoolImpl(DRIVER, URL, USER_NAME, PASSWORD,
-						INITIAL_CONNECTIONS, MAX_CONNECTIONS, WAIT_IF_BUSY);
-				UtilisateurPoolConnexionThead uPool = new UtilisateurPoolConnexionThead(NB_QUERY, pool);
-				uPool.start();
+		
+		int nbConnexionPool = 0;
+		beginGlobal = System.currentTimeMillis();
+		try {
+			CustomConnectionPoolImpl pool = new CustomConnectionPoolImpl(DRIVER, URL, USER_NAME, PASSWORD,
+					INITIAL_CONNECTIONS, MAX_CONNECTIONS, WAIT_IF_BUSY);
+			pool.run();
+			beginGlobal = System.currentTimeMillis();
+			for (int i = palier; i <= MAX_CONNECTIONS ; i += palier) {
+				begin = System.currentTimeMillis();
+				List<Connection> conn = new ArrayList<Connection>();
+				for(int j = 0 ; j < i ; j++) {
+					conn.add(pool.getConnection()); // Prends la connexion par référence du pool
+				}
+				for (int k = 0 ; k < conn.size() ; k++) {
+					if(UTILISER_THREAD) {
+						Thread threadConnexion = new Thread(new ConnexionThreadTest(pool, conn.get(k)));
+						threadConnexion.run();
+					}
+					else {
+						PreparedStatement ps = conn.get(k).prepareStatement("SELECT * FROM facebook.utilisateur");
+						ResultSet res = ps.executeQuery();
+						res.close();
+						pool.releaseConnection(conn.get(k)); // Renvoi de la connexion vers le pool
+					}
+				}
 				
-				System.out.print(".");
+				end = System.currentTimeMillis();
+				conn = null;
+				nbConnexionPool++;
+				//long dureeEtape = end-begin;
+				//dureesConnexions.add(new HashMap<Integer, Long>(Integer.valueOf(i), Long.valueOf(dureeEtape)));
+				System.out.println("Pool avec : " + i + " connexions a durée : " + (end - begin));
+				
 			}
-		
-		
+			endGlobal = System.currentTimeMillis();
+			pool = null;
+			System.out.println("Temps total des opérations d'exécution des " + nbConnexionPool + " pools a duré " + (endGlobal - beginGlobal));
 		}catch(SQLException e){
 			e.printStackTrace();
-
-		}finally{
-
-			end = System.currentTimeMillis();
-
-			System.out.println();
-			System.out.println(  "Le nombre d'utilisateur est "   + NB_UTISATEUR );
-			System.out.println(  "Le nombre de requete envoyée par chaque utilsateur est " + NB_QUERY );
-			System.out.println(  "Le Temps mit pour envoyer les "  + (NB_QUERY *  NB_UTISATEUR) +  "est "   + (end - begin) + "ms" );
-			System.out.println(  "En moyenne, chaque requete met "  +  (end - begin) / (NB_QUERY  * NB_UTISATEUR) +  " ms");
-
 		}
-
 	}
-
-
-
+	
+	/**
+	 * Pour obtenir les infos afin de dessiner le graph et à recupérer via ajax
+	 * @throws Exception 
+	 */
+	public ArrayList<HashMap<Integer, Long>> getDureesConnexions() throws Exception {
+		if(dureesConnexions != null) {
+			return dureesConnexions;
+		}
+		throw new Exception();
+	}
 }
